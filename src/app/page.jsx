5f5b9dc3a71b824e905602a4ce6a5f90398992e9
ihsanannashir <ihsanannashir@gmail.com>
2023-04-27 11:43:34 +0700

    setup config and frameworks

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-5xl px-4 sm:px-6 md:max-w-full bg-[#1C1B33]">
+      <div>Halo sanak</div>
+    </main>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { IoMdMenu, IoMdClose } from 'react-icons/io'
+
+function Navbar() {
+  return (
+    <header className='w-full mx-auto px-4 shadow fixed top-0 z-50 sm:px-20 bg-[#2E335A]'>
+        <div className='justify-between md:items-center md:flex'>
+            <div>
+                ini navigasi
+            </div>
+            <div>
+                ini buat search
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
