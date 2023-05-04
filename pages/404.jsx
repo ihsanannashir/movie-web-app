b6f366ff72e9ebc0fca8c30b8b8733f9c0ee99bb
ihsanannashir <ihsanannashir@gmail.com>
2023-05-04 16:32:41 +0700

    add 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,3 +1,25 @@
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Link from "next/link";
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found hey</h1>
+    return <>
+    <Head className=''>
+        <title>Page Not Found</title>
+        <meta name="description" content='404 - Page Not Found' />
+        <link rel="icon" href="/icon.png" />
+    </Head>
+    <div className="fixed top-0"><Navbar Title="Page Not Found"/></div>
+    <main className="h-screen bg-dark-5">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center space-y-4">
+          <div className="text-xl md:text-4xl font-bold">We've lost this page</div>
+          <div className="text-xs md:text-base font-medium">Sorry, the page you are looking for doesn't exist or has been moved.</div>
+          <Link href={'/'} className="bg-dark-2 hover:bg-dark-1 text-xs md:text-base px-4 py-2 font-semibold rounded-lg">Back to Home</Link>
+        </div>
+      </div>
+    </main>
+    <div className="fixed bottom-0 w-screen"><Footer /></div>
+    </>
   }
