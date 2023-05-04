3e659f25d9ff63645766fcccc36d9de123241f89
ihsanannashir <ihsanannashir@gmail.com>
2023-05-04 17:20:19 +0700

    setup route for genres page

--- pages/genres/[id].jsx
+++ pages/genres/[id].jsx
@@ -1,9 +1,57 @@
+import Layout from '@/components/layout'
 import React from 'react'
+import { SERVER, API_KEY } from '@/config'
+import GenreSlider from '@/components/GenreSlider'
+import { useRouter } from 'next/router'
 
-function GenrePage() {
+function GenrePage({dataLatest, dataGenreId}) {
+  const router = useRouter()
+  const { id } = router.query
+
+  const name = dataGenreId.genres.map((item) => {
+    if (id.toString() === item.id.toString()) {
+      const genName = item.name
+      return genName
+    }
+  })
+
   return (
-    <div>Genre Details Page under construction</div>
+    <Layout pageTitle={'Duar'}>
+      <GenreSlider data={dataGenreId}/>
+      <div>{dataLatest.results[0].original_title}</div>
+      <div>this is {name} page</div>
+    </Layout>
   )
+}
+
+export async function getStaticPaths() {
+  const resPath = await fetch(`${SERVER}/genre/movie/list?api_key=${API_KEY}`)
+  const dataPath = await resPath.json()
+
+  const paths = dataPath.genres.map((gen) => ({
+    params: {
+        id: `${gen.id}`
+    },
+  })) 
+
+  return {
+      paths,
+      fallback: 'blocking',
+  };
+}
+
+export async function getStaticProps({params}) {
+  const resGenreId = await fetch(`${SERVER}/genre/movie/list?api_key=${API_KEY}`)
+  const dataGenreId = await resGenreId.json()
+
+  const resLatest = await fetch(`${SERVER}/discover/movie?with_genres=${params.id}&sort_by=release_date.desc&api_key=${API_KEY}`)
+  const dataLatest = await resLatest.json()
+
+  return {
+    props: {
+      dataLatest, dataGenreId,
+    },
+  }
 }
 
 export default GenrePage
